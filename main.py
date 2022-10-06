eab5a145398c6708329e2e9991bb5d4b6665a2ee
Adam De Rango <113118048+adamdera@users.noreply.github.com>
2022-10-06 10:00:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("how old are you?:"))
+
+if age >= 17 :
+  print ("you can see an R rated movie")
+
+elif age >= 13 :
+  print ("you can see a PG rated movie")
+
+else :
+  print ("you can see a G rated movie")
